04fa23940c13eb35ab37de5bcc3dfcfd590717d1
Mohammedadn <mohammedaden023@gmail.com>
2024-05-28 09:11:24 +0300

    test

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    num1 = 45
+    num2 = 55
+    sum_number = num1 + num2
+    print(sum_number)
+    return sum_number
     pass
 
 def halve(number):
+    return number/2
     pass
